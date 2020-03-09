08073b6f5351526163063f4b127f23a22638260e
Ryan Lancione <ryan@Ryans-MacBook-Pro-5.local>
2020-03-09 12:50:16 -0700

    added argparse and changed to PDF output

--- motmark_main.py
+++ motmark_main.py
@@ -8,68 +8,27 @@ import random
 import numpy as np
 import imshow
 from matplotlib import cm
+import argparse
+
+# Argparse implementation
+def get_args():
+    parser = argparse.ArgumentParser(description="A program to find and illustrate motifs")
+    parser.add_argument("-i", help="Input fasta file", required = True)
+    parser.add_argument("-o", help="Output image file", required = True)
+    parser.add_argument("-m", help="file with input motifs", required = True)
+    return parser.parse_args()
+
+args = get_args()
 
 
 
-
-# to notes
-# remove upper case in motifs & convert u's to c's.
-# remove upper case in exons after identified and drawn
-# ensure regex list length == number of motifs
-# Find out why regex arent fitting
-#
-
-# Read in files
-m21fa = open("m21.fa")
-
-# use seqe lengths to calculate width of surface
-# use num_seqs to calculate height
-seq_lens = ()
-num_seqs = 0
-for i, line in enumerate(m21fa):
-    #print(len(line))
-    if i == 1:
-        line2 = line
-    if i == 3:
-        line3 = line
-    if i == 5:
-        line4 = line
-    if line[0] == ">":
-        num_seqs += 1
-
-
-m21fa.close()
-#seqs = open("Figure_1.fasta")
-
-# read in motifs, standardize them
-mots = set()
-col_palate = []
-with open("../Fig_1_motifs.txt", "r") as fh:
-    for i,mot in enumerate(fh):
-        mot = mot.replace("G","g")
-        mot = mot.replace("C","c")
-        mot = mot.replace("T","t")
-        mot = mot.replace("A","a")
-        mot = mot.replace("U","u")
-        mot = mot.replace("Y","y")
-        mot = mot.replace("u","c")
-        mots.add(mot.strip())
-
-        # create colors for each motif
-        #col_palate.append()
-        col = [random.random(),random.random(),random.random()]
-        col_palate.append(col)
-
-print(col_palate)
-#print(mots)
-
-
+##################
 # Functions
+##################
 
-'''
 def mto2linefasta(file):
     file = open(file)
-    nfile =  open("m21.fa", "w")
+    nfile =  open(f"{args.i}.2lf", "x")
     for i,line in enumerate(file):
         if line[0] == ">" and i == 0:
             nfile.write(line)
@@ -78,30 +37,19 @@ def mto2linefasta(file):
         if line[0] != ">":
             nfile.write(line.strip())
 
-mto2linefasta("../Figure_1.fasta")
 
-'''
 
 
 def draw_intron(y, seq):
     # identify introns
-    intron = re.compile('[a-z]+')
-    j = intron.findall(seq)
+    seq_len = len(seq)
 
-    # loop over introns and draw line
-    ctx.set_line_width(5)
+    # draw line
     ctx.set_source_rgb(0,0,0)
-    for match in j:
-        # identify intron coordinates
-        i = re.compile(match)
-        f = i.search(seq)
-        start, end = f.span()
-
-        # draw line
-        ctx.move_to(start +30,y)
-        ctx.line_to(end +30,y)
-        ctx.stroke()
-
+    ctx.set_line_width(5)
+    ctx.move_to(30,y)
+    ctx.line_to(seq_len +30,y)
+    ctx.stroke()
 
 
 def draw_exon(y, seq):
@@ -109,30 +57,34 @@ def draw_exon(y, seq):
     intron = re.compile('[A-Z]+')
     j = intron.findall(seq)
 
-    # loop over exons and draw line
-    ctx.set_line_width(10)
+    # loop over exons and draw rectangles
+    ctx.set_line_width(3)
     ctx.set_source_rgb(0,0,0)
     for match in j:
         # identify intron coordinates
         i = re.compile(match)
         f = i.search(seq)
         start, end = f.span()
+        #print(start,end)
 
-        # draw line
-        ctx.move_to(start +30,y)
-        ctx.line_to(end +30,y)
+        ctx.rectangle(start+30, y-15,end-start, 30)
         ctx.stroke()
 
 
 
 
+########
+#return a dict with motif color pairs
+#########
 
-
+mot_col = dict()
 def draw_mots(y, seq):
 
     # Create regex for the input motifs
     for i,m in enumerate(mots):
         mot_len = len(m)
+        cols = col_palate[i]
+        mot_col.update({m : cols})
         if "y" in m:
             m = m.replace("y", "[ct]{1}")
         if "u" in m:
@@ -144,12 +96,7 @@ def draw_mots(y, seq):
 
         # Color the motifs. Set Width
         ctx.set_line_width(20)
-#        cols = cm.jet(random.randint(1,100))[0:3]
-#        ctx.set_source_rgb(cols[0],cols[1],cols[2])
-        cols = col_palate[i]
-        print(cols[0])
-        print(cols[1])
-        print(cols[2])
+
         ctx.set_source_rgb(cols[0],cols[1],cols[2])
         for match in j:
             # calculate end manually. len motif -1
@@ -160,53 +107,89 @@ def draw_mots(y, seq):
             ctx.move_to(start +30,y)
             ctx.line_to(end+30,y)
             ctx.stroke()
-#        print(cols)
-#
-    #for i in range(100):
-        #fprint(cm.jet(i))
+
+        #print(m, cols)
 
 
-def draw_text(y,text):
+def draw_text(x,y,text):
     ctx.select_font_face("Purisa", cairo.FONT_SLANT_NORMAL,
         cairo.FONT_WEIGHT_NORMAL)
     ctx.set_source_rgb(0,0,0)
     ctx.set_font_size(13)
 
-    ctx.move_to(30, y)
+    ctx.move_to(x, y)
     ctx.show_text(text)
+
+
+###################
+#File and Variable setup
+###################
+
+
+# Create 2 line 2 line fasta
+mto2linefasta(args.i)
+
+
+# Read in seqs
+# use seqe lengths to calculate width of surface
+# use num_seqs to calculate height
+with open(f"{args.i}.2lf") as fh:
+    seq_lens = ()
+    num_seqs = 0
+    for i, line in enumerate(fh):
+        if line[0] == ">":
+            num_seqs += 1
+
+
+
+# read in motifs, standardize them to lower case
+mots = set()
+col_palate = []
+with open(args.m, "r") as fh:
+    for i,mot in enumerate(fh):
+        mot = mot.replace("G","g")
+        mot = mot.replace("C","c")
+        mot = mot.replace("T","t")
+        mot = mot.replace("A","a")
+        mot = mot.replace("U","u")
+        mot = mot.replace("Y","y")
+        mot = mot.replace("u","c")
+        mots.add(mot.strip())
+
+        # create colors for each motif
+        col = [random.random(),random.random(),random.random()]
+        col_palate.append(col)
+
 
 
 # Pycairo surface setup
 
-WIDTH, HEIGHT = 900, num_seqs * 120
+WIDTH, HEIGHT = 900, num_seqs * 120 + 25
 
-surface = cairo.ImageSurface(cairo.FORMAT_ARGB32, WIDTH, HEIGHT)
+#surface = cairo.ImageSurface(cairo.FORMAT_ARGB32, WIDTH, HEIGHT)
+surface =cairo.PDFSurface(f"{args.o}", WIDTH, HEIGHT)
 ctx = cairo.Context(surface)
 
-'''
-ctx.set_line_width(5)
-ctx.move_to(0,10)
-ctx.line_to(500,10)
-ctx.stroke()
 
-ctx.move_to(0,100)
-ctx.line_to(300,100)
-#ctx.rectangle(0, 0, 20, 20)
-ctx.stroke()
-#ctx.rectangle(x,y,width,height)
-'''
+# Text Header
+ctx.select_font_face("Tango", cairo.FONT_SLANT_NORMAL,
+    cairo.FONT_WEIGHT_NORMAL)
+ctx.set_source_rgb(0,0,0)
+ctx.set_font_size(25)
+
+ctx.move_to(400, 20)
+ctx.show_text("Motif-Mark")
 
 
 # To scale intron Y coords
-
 scale_list = list()
 for i in range(num_seqs):
-    scale_list.append(HEIGHT/num_seqs * i + 50)
+    scale_list.append(HEIGHT/num_seqs * i + 75)
 
 
 
 # Main Loop
-m21fa = open("m21.fa")
+m21fa = open(f"{args.i}.2lf")
 cntr = 0
 while True:
     label = m21fa.readline()
@@ -215,12 +198,12 @@ while True:
     seq = m21fa.readline()
 
 
-    print(cntr)
+    #print(cntr)
     y_coord = scale_list[cntr]
 
 
     draw_intron(y_coord, seq)
-    draw_text(y_coord - 30, label)
+    draw_text(30, y_coord - 30, label)
     draw_exon(y_coord, seq)
     seq = seq.replace("G","g")
     seq = seq.replace("C","c")
@@ -229,25 +212,48 @@ while True:
     draw_mots(y_coord, seq)
     cntr +=1
 
+m21fa.close()
 
 
 
 
-'''
-draw_intron(30, line2)
-draw_intron(130, line3)
-draw_intron(260, line4)
 
-draw_exon(30, line2)
-line2 = line2.replace("G","g")
-line2 = line2.replace("C","c")
-line2 = line2.replace("T","t")
-line2 = line2.replace("A","a")
-draw_mots()
-#print(line2)
 
-'''
+# Create Legend
+#ctx.rectangle(x, y, width, height)
+ctx.set_line_width(2)
+ctx.set_source_rgb(0,0,0)
+ctx.rectangle(740, 50, 130, len(mots)*25 +25)
+ctx.stroke()
+
+
+# scale for y
+lgnd_scale = list()
+for i in range(len(mots)):
+    lgnd_scale.append(20 * i + 65)
+
+
+#draw color blocks and print text
+for i,mot in enumerate(mots):
+    draw_text(800,lgnd_scale[i]+8, mot)
+    ctx.set_line_width(2)
+    col = mot_col.get(mot)
+    ctx.set_source_rgb(col[0],col[1],col[2])
+    ctx.rectangle(759, lgnd_scale[i],10 , 10)
+    ctx.fill()
+    ctx.stroke()
+
+# add exon to legend
+draw_text(800, lgnd_scale[len(lgnd_scale)-1]+28, "exon")
+ctx.set_line_width(2)
+ctx.set_source_rgb(0,0,0)
+ctx.rectangle(759, lgnd_scale[len(lgnd_scale)-1]+20,10 , 10)
+ctx.stroke()
+
+
 
 
 # Save to file
-surface.write_to_png("example.png")  # Output to PNG
+#surface.write_to_png(f"{args.o}.png")  # Output to PNG
+#ctx.show_page()
+surface.finish()
